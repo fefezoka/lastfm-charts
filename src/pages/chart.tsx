cf81d089b20eac9cea225768a49d57c63890f880
feripe1 <89111098+fefezoka@users.noreply.github.com>
2023-05-21 14:17:36 -0300

    css changes

--- src/pages/chart.tsx
+++ src/pages/chart.tsx
@@ -39,96 +39,102 @@ export default function Profile() {
   }
 
   return (
-    <Box as={'main'} css={{ m: 'auto', width: 'fit-content' }} id="page">
-      <Flex justify={'between'} align={'center'} css={{ bc: '$bg2', py: '$2', px: '$6' }}>
-        <Box>
-          <Text weight={600}>
-            {type || 'albums'}, {format || '5x5'}, {period || '7day'}
-          </Text>
-        </Box>
-        <Flex justify={'center'} align={'center'} gap={'6'}>
-          <ProfileIcon src={user.image[3]['#text']} alt="" css={{ size: 56 }} />
-          <Heading>{user.name}</Heading>
-          <Flex direction={'column'} align={'center'}>
-            <Text weight={600}>{user.playcount}</Text>
-            <Text>scrobbles</Text>
+    <Box as={'main'} css={{ height: '100vh' }}>
+      <Box css={{ width: '100%', bc: '$bg2', mb: '$4' }}>
+        <Flex
+          justify={'between'}
+          align={'center'}
+          css={{ py: '$2', px: '$6', maxWidth: 768, m: 'auto' }}
+        >
+          <Box>
+            <Text weight={600}>
+              {type || 'albums'}, {format || '5x5'}, {period || '7day'}
+            </Text>
+          </Box>
+          <Flex justify={'center'} align={'center'} gap={'6'}>
+            <ProfileIcon src={user.image[3]['#text']} alt="" css={{ size: 56 }} />
+            <Heading>{user.name}</Heading>
+            <Flex direction={'column'} align={'center'}>
+              <Text weight={600}>{user.playcount}</Text>
+              <Text>scrobbles</Text>
+            </Flex>
+          </Flex>
+          <Flex as={'button'} onClick={() => router.push('/')}>
+            <BsArrowLeft size={36} />
           </Flex>
         </Flex>
-        <Flex as={'button'} onClick={() => router.push('/')}>
-          <BsArrowLeft size={36} />
-        </Flex>
-      </Flex>
+      </Box>
 
-      {img ? (
-        <Image
-          src={img}
-          alt=""
-          width={170 * Number(format.charAt(0))}
-          height={170 * Number(format.charAt(2))}
-        />
-      ) : (
-        <Grid
-          id="chart"
-          ref={ref}
-          css={{
-            m: 'auto',
-            gridTemplateColumns: `repeat(${format.substring(
-              0,
-              format.indexOf('x')
-            )}, 1fr)`,
-            position: 'relative',
-          }}
-        >
-          <Flex
-            id={'loading'}
-            justify={'center'}
-            align={'center'}
-            data-html2canvas-ignore
-            css={{
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-              zIndex: 999,
-              backdropFilter: 'blur(3px)',
-            }}
+      <Box css={{ m: 'auto', width: 'fit-content' }}>
+        {img ? (
+          <Image
+            src={img}
+            alt=""
+            width={170 * Number(format.charAt(0))}
+            height={170 * Number(format.charAt(2))}
           />
-          {chart.chart.map((item: any, index: any) => (
-            <Flex key={index} css={{ position: 'relative' }}>
-              <Image
-                src={item.image[3]['#text']}
-                alt=""
-                height={170}
-                width={170}
-                quality={100}
-                priority
-              />
-              <Flex
-                direction={'column'}
-                align={'center'}
-                justify={'end'}
-                css={{
-                  pb: '$1',
-                  position: 'absolute',
-                  bottom: 0,
-                  width: '100%',
-                  height: '40%',
-                  ta: 'center',
-                  background: 'linear-gradient(transparent, $blackA10)',
-                }}
-              >
-                {type !== 'artists' && (
+        ) : (
+          <Grid
+            ref={ref}
+            css={{
+              gridTemplateColumns: `repeat(${format.substring(
+                0,
+                format.indexOf('x')
+              )}, 1fr)`,
+              position: 'relative',
+            }}
+          >
+            <Flex
+              id={'loading'}
+              justify={'center'}
+              align={'center'}
+              data-html2canvas-ignore
+              css={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                zIndex: 999,
+                backdropFilter: 'blur(3px)',
+              }}
+            />
+            {chart.chart.map((item: any, index: any) => (
+              <Flex key={index} css={{ position: 'relative' }}>
+                <Image
+                  src={item.image[3]['#text']}
+                  alt=""
+                  height={170}
+                  width={170}
+                  quality={100}
+                  priority
+                />
+                <Flex
+                  direction={'column'}
+                  align={'center'}
+                  justify={'end'}
+                  css={{
+                    pb: '$1',
+                    position: 'absolute',
+                    bottom: 0,
+                    width: '100%',
+                    height: '40%',
+                    ta: 'center',
+                    background: 'linear-gradient(transparent, $blackA10)',
+                  }}
+                >
+                  {type !== 'artists' && (
+                    <Text size={'0'} weight={600}>
+                      {item.artist.name}
+                    </Text>
+                  )}
                   <Text size={'0'} weight={600}>
-                    {item.artist.name}
+                    {item.name}
                   </Text>
-                )}
-                <Text size={'0'} weight={600}>
-                  {item.name}
-                </Text>
+                </Flex>
               </Flex>
-            </Flex>
-          ))}
-        </Grid>
-      )}
+            ))}
+          </Grid>
+        )}
+      </Box>
     </Box>
   );
 }
